4dd7d524aebe79adbe0e6a9e38fe98367c63103a
Asthere0n <cristiansanchezsonido@gmail.com>
2024-10-09 17:51:56 +0100

    Sorted numeric pad functionality

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-//Initial state of the display
-let displayScreen = document.querySelector('#screen').value
-console.log(displayScreen)
+//Initial state of the calculator
+let currentScreen = document.querySelector('#screen').value.split('')
+console.log(currentScreen)
+let numberInMemory = 0
 
-//
+//Function that will transform the array and print in screen as string 
+const writeInScreen = function (array) {
+    currentScreen = array
+    document.querySelector('#screen').value = array.join('')
+}
+
+//Functionality of Reset button
+document.querySelector('#buttonReset').addEventListener('click', () => {
+    console.log('Reset')
+    writeInScreen([])
+})
+
+//functionality of C button
+document.querySelector('#buttonDelete').addEventListener('click', () => {
+    console.log('Delete')
+    currentScreen.pop()
+    writeInScreen(currentScreen)
+})
+
+//Numeric pad
+const numPad = document.querySelectorAll('.buttonNum')
+numPad.forEach((numButton)=>{
+    numButton.addEventListener('click', ()=>{
+        console.log(numButton.value)
+        currentScreen.push(numButton.value)
+        writeInScreen(currentScreen)
+    })
+}) 
